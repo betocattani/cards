0410790a5d2f59badb91a160747e811b33d3446b
Cattani <contato.cattani@gmail.com>
2020-06-28 20:01:32 -0300

    creates documentation to module and method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Providers methods to creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Queen", "Jack", "king"]
     suits = ["Spades", "Clubs", "Heart", "Diamond"]
@@ -26,6 +33,18 @@ defmodule Cards do
   end
 
   # Cards.deal(deck, 5) { *hand*, *deck* } - Tuple
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be  in the  hand.
+
+    ## Examples
+
+          iex> deck = Cards.create_deck
+          iex> { hand, deck } = Cards.deal(deck, 1)
+          iex> hand
+          ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
